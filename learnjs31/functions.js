376d09221cde64e2012939cff703f46c1e8a4b96
G Thimmaraju <thimmaraju418@gmail.com>
2025-09-25 11:47:32 +0530

    arrow functions

--- learnjs31/functions.js
+++ learnjs31/functions.js
@@ -1,14 +1,6 @@
 
 
-function add(num1, num2){
 
-    return num1+ num2
-}
-
-
-x = add(5,6)
-
-console.log(x)
 // function with parameters / arguments
 // function without parameters  / arguments
 
@@ -17,3 +9,77 @@ console.log(x)
 
 // Raju
 
+
+
+// function displayMessage(){
+
+//     console.log("This is a simple Function")
+//     console.log("2nd statement in the same function")
+// }
+
+// displayMessage()
+
+displayMessage = () => {
+    console.log("This is a simple Function")
+    console.log("2nd statement in the same function")
+
+}
+
+
+displayMessage()
+
+
+// function  Multuply(num1, num2){
+
+//     console.log(num1)
+
+//     console.log(num2)
+
+//     console.log(num1*num2)
+// }
+
+// Multuply(6,9)
+
+
+Multuply = (num1, num2) => {
+
+    console.log(num1)
+
+    console.log(num2)
+
+    console.log(num1*num2)
+}
+
+Multuply(2,8)
+
+
+
+// function add(num1, num2){
+
+//     return num1+ num2
+// }
+
+
+// x = add(5,6)
+
+// console.log(x)
+
+
+add = (num1, num2) => num1+ num2
+
+x = add(4,7)
+
+console.log(x)
+
+
+// function displayname(str){
+
+//     console.log("Hello " +str )
+// }
+
+// displayname("Padma sree")
+
+
+displayname = str => console.log("Hello " +str )
+
+displayname("Padma sree")
